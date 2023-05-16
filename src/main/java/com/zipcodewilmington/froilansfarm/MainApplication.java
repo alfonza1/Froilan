23a8a334149a3eed9c94c6aa7d7e73c6086de1d6
Your Name <you@example.com>
2023-05-16 11:52:08 -0400

    testing

--- src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
@@ -4,5 +4,7 @@ package com.zipcodewilmington.froilansfarm;
  * Created by leon on 2/26/18.
  */
 public class MainApplication {
-
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
 }
