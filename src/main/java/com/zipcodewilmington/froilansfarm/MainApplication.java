d8d39d1ce9659595eafb0f43836eb8d1234e0b49
Angel Borja <borjaangel244@gmail.com>
2023-05-16 14:55:29 -0400

    testit

--- src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
@@ -6,7 +6,7 @@ package com.zipcodewilmington.froilansfarm;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println();
+        System.out.println("yo");
 
 
 
@@ -30,8 +30,5 @@ public class MainApplication {
 
     }
 
-    public static void main(String[] args) {
-
-    }
 
 }
