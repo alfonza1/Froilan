d6d1b43fd5c39abdfff9823fbecbcfdf072117ac
Jaimi Estrada <jaimiestrada007@gmail.com>
2023-05-19 14:37:25 -0400

    nothing

--- src/main/java/classes/Pilot.java
+++ src/main/java/classes/Pilot.java
@@ -1,10 +1,10 @@
 package classes;
 
 import classes.Person;
+import interfaces.Rider;
 
-public class Pilot extends Person implements Rider{
-    public Pilot(String name) {
-        super(name);
+public class Pilot extends Person implements Rider {
+    public Pilot(String name) {super(name);
     }
 
 }
