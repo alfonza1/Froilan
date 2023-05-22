ef8e8eea12f3ceaac6233634d5969a769196d475
Your Name <you@example.com>
2023-05-21 20:18:23 -0400

    small changes

--- src/main/java/classes/Chicken.java
+++ src/main/java/classes/Chicken.java
@@ -3,6 +3,9 @@ package classes;
 import interfaces.Edible;
 import interfaces.Produce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chicken extends Animal implements Produce {
     private boolean hasBeenFertilized;
 
@@ -21,13 +24,15 @@ public class Chicken extends Animal implements Produce {
 
     }
 
-    public EdibleEgg yield() {
-        if (!hasBeenFertilized) {   //checks if egg has been fertilized or not - if false - returns an edible egg
-            return new EdibleEgg();
-        } else {
-            return null;
+    public void yield() {
+        List<Edible> bucket = new ArrayList<>();
+
+        bucket.add(new EdibleEgg());
+
+        System.out.println(bucket);
+
         }
-    }
+
 
     @Override
     public void eat(Edible edible) {
